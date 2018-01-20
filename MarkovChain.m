20d162efcf79168ac4ce9d2bfea266c52369599c
sooonia <kopels@g.cofc.edu>
2018-01-20 15:25:37 -0500

    Fixed Markov Chain Script
    variable names p2 and p5 were reused for different object

--- MarkovChain.m
+++ MarkovChain.m
@@ -14,12 +14,12 @@ matorig = [.08*p1, .02*p1, .16*p1, .24*p1, .07*p1, .41*p1, .02*p1, 1-p1;
     .08*p5, .02*p5, .16*p5, .24*p5, .07*p5, .41*p5, .02*p5, 1-p5;
     .08*p6, .02*p6, .16*p6, .24*p6, .07*p6, .41*p6, .02*p6, 1-p6;
     .08*p7, .02*p7, .16*p7, .24*p7, .07*p7, .41*p7, .02*p7, 1-p7;
-    0, 0, 0, 0, 0, 0, 0, 1;];
+    0, 0, 0, 0, 0, 0, 0, 1];
 newmat = eye(8);
 mat = matorig;
 i=1;
 %determining number of iterations for initial model to reach equalibrium
-e = 0.0005 %feel free to change this
+e = 0.00005 %feel free to change this
 while(not(sum(sum(newmat - mat < e))==64))
     mat=newmat;
     newmat = mat*matorig;
@@ -29,14 +29,14 @@ end
 i
 newmat
 %calculating n-step matrices
-p2 = matorig^2;
-p5 = matorig^5;
-p10 = matorig^10;
-p30 = matorig^30;
-p2
-p5
-p10
-p30
+p2v1 = matorig^2;
+p5v1 = matorig^5;
+p10v1 = matorig^10;
+p30v1 = matorig^30;
+p2v1
+p5v1
+p10v1
+p30v1
 
 %what if 1 ... don't switch methods
 matorig2 = [.78, 0, 0, 0, 0, 0, 0, .22; 
@@ -70,8 +70,8 @@ p10v2
 p30v2
 
 %what if 2... cycle pregant people back in with fresh start
-matorig3 = [.08*p1, .02*p1, .16*p1, .24*p1, .07*p1, .41*p1, .02*p1, 1-p1;
-    .08*p2, .02*p2, .16*p2, .24*p2, .07*p2, .41*p2, .02*p2, 1-p2;
+matorig3 = [.08*p1, .02*p1, .16*p1, .24*p1, .07*p1, .41*p1, .02*p1, 1-p1
+    .08*p2, .02*p2, .16*p2, .24*p2, .07*p2, .41*p2, .02*p2, 1-p2
     .08*p3, .02*p3, .16*p3, .24*p3, .07*p3, .41*p3, .02*p3, 1-p3;
     .08*p4, .02*p4, .16*p4, .24*p4, .07*p4, .41*p4, .02*p4, 1-p4;
     .08*p5, .02*p5, .16*p5, .24*p5, .07*p5, .41*p5, .02*p5, 1-p5;
@@ -100,7 +100,3 @@ p5v3
 p10v3
 p30v3
 
-
-%rowNames = {'a','b','c'};
-%colNames = {'x','y','z'};
-%sTable = array2table(sample,'RowNames',rowNames,'VariableNames',colNames)
