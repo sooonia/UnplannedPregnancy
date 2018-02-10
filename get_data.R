d58c272ef6773c5f4a47f984b43e607239705663
Amber Widmer <amberplehman@g.cofc.edu>
2018-02-10 13:42:27 -0500

    added test

--- get_data.R
+++ get_data.R
@@ -10,3 +10,11 @@ intended.bp <- droplevels(intended[intended$QuestionId == 'QUO130',])
 untab <- aggregate(unintended.bp$Sample_Size, by = list(Response = unintended.bp$Response), FUN = sum)
 tab <- aggregate(intended.bp$Sample_Size, by = list(Response = intended.bp$Response), FUN = sum)
 
+x<-c(untab[2,2],tab[2,2])
+dfun<-as.data.frame(untab)
+as.numeric(dfun$x)
+df<-as.data.frame(tab)
+as.numeric(df$x)
+n<-c(sum(dfun$x),sum(df$x))
+prop.test(x,n)
+    
