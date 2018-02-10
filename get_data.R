0fcea57e36b70ea237ecb83b74c23e6ca0e15af9
sooonia <kopels@g.cofc.edu>
2018-02-10 13:20:10 -0500

    Beginning pipeline to compare un vs intended

--- get_data.R
+++ get_data.R
@@ -1,2 +1,12 @@
+load('data.rda')
 
-rd <- read.csv("CDC_PRAMStat_Data_for_2011.csv")
+unintended <- rd[rd$Break_Out == 'Unintended',]
+intended <- rd[rd$Break_Out == 'Intended',]
+
+
+unintended.bp <- droplevels(unintended[unintended$QuestionId == 'QUO130',])
+intended.bp <- droplevels(intended[intended$QuestionId == 'QUO130',])
+
+untab <- aggregate(unintended.bp$Sample_Size, by = list(Response = unintended.bp$Response), FUN = sum)
+tab <- aggregate(intended.bp$Sample_Size, by = list(Response = intended.bp$Response), FUN = sum)
+
